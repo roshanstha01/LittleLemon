54557d7369bedefb630f0206c722be1a7f0c90b1
roshanstha01 <roshanstha958737@gmail.com>
2026-05-31 18:50:04 +0545

    LittleLemon models and APIs

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import generics, viewsets
+
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+
+
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', {'current_year': date.today().year})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
